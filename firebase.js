9954c4a1762536989ea960cee28d0c877b50b760
Manoj Das <dasmj4321@gmail.com>
2025-05-13 12:03:40 +0530

    env files removed

--- firebase.js
+++ firebase.js
@@ -1,22 +1,21 @@
+require('dotenv').config();
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-import { getStorage } from "firebase/storage"
-
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCYQUwbvCMTeDAffYtr7PvGGk00pa2sqJ8",
-  authDomain: "wa-clone-5412d.firebaseapp.com",
-  projectId: "wa-clone-5412d",
-  storageBucket: "wa-clone-5412d.appspot.com",
-  messagingSenderId: "130953297802",
-  appId: "1:130953297802:web:0d6f643c3c15e1d5a69273"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
-
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore();
 const storage = getStorage();
 
-export { auth, db, storage }
+export { auth, db, storage };
